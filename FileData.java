5d5fa39ca33b04edbdc0e641a6adce8ae4e737fd
Ramtin Kazemi <ramkazemi@yahoo.com>
2023-06-25 13:35:07 -0700

    Create FileData.java

---
+++ FileData.java
@@ -0,0 +1,16 @@
+public class FileData {
+
+    public String name;
+    public String dir;
+    public String lastModifiedDate;
+
+    public FileData(String name, String directory, String modifiedDate) {
+        this.name = name;
+        this.dir = directory;
+        this.lastModifiedDate = modifiedDate;
+    }
+
+    public String toString() {
+        return "{Name: " + name + ", Directory: " + dir + ", Modified Date: " + lastModifiedDate + "}";
+    }
+}
